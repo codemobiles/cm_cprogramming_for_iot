68c33bb74a406b8010e875ed7b34975e32262e7f
codemobiles <chaiyasit.t@gmail.com>
2020-07-28 20:14:04 +0700

    demo6_variable_float_char

---
+++ demo6_variable_float_char/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // data type - integer
+    int money = 10;
+    int a = 20;
+    int b = 5;
+    
+    float moneyFloat = 10.123;
+    char letter = 'b';
+    
+    
+    printf("money=%d, a=%d, b=%d, moneyFloat=%.3f, letter=%c",money,a,b,moneyFloat,letter );
+    
+    
+    return 0;
+}
