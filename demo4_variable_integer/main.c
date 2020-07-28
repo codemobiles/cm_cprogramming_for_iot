6f6fa37937a9dd88473315f430bfa06101f3d14f
codemobiles <chaiyasit.t@gmail.com>
2020-07-28 17:08:11 +0700

    demo4_variable_integer

---
+++ demo4_variable_integer/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // data type - integer
+    int money;
+    int a;
+    int b;
+    
+    float moneyFloat;
+    char letter;
+    
+    money = 10;
+    a = 20;
+    b = 5;
+    
+    printf("money=%d, a=%d, b=%d",money,a,b );
+    
+    return 0;
+}
