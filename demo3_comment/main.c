527c5bf0b527707b8b695fd773a87f2e256e5f7f
codemobiles <chaiyasit.t@gmail.com>
2020-07-28 16:49:41 +0700

    demo3_comment

---
+++ demo3_comment/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+void display(){
+    printf("Begin\n");
+}
+*/
+
+
+// แสดงข้อความ
+void display(){
+    printf("Begin\n");
+}
+
+
+int main()
+{
+    display();
+    return 0;
+}
