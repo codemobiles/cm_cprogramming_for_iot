60d868999c8f944ac3ff718de06c60d04b978232
codemobiles <chaiyasit.t@gmail.com>
2020-07-28 20:05:50 +0700

    demo5_variable_naming

---
+++ demo5_variable_naming/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    // data type - integer
+    int money;
+    int a;
+    int b;
+    
+    float moneyFloat;
+    char letter;
+    
+    money = 10;
+    a = 20;
+    b = 5;
+    
+    printf("money=%d, a=%d, b=%d",money,a,b );
+    
+    // a-z A-Z 1234
+    int iiI1 ;
+    
+    return 0;
+}
